fe81c7a912d174f6864ae298788d3f59f547f240
ajaykumar2pp <ajay2kumarpp@gmail.com>
2024-10-06 14:12:49 +0530

    fix bugs

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express');
+const cors = require('cors')
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger/swagger.json');
 const dbConfig = require('./src/config/db.config')
@@ -12,22 +13,27 @@ dbConfig.connectMongoDB();
 //********* Middleware   ***********/ 
 app.use(express.json());
 
+//*********** Enable CORS for all routes **************// 
+app.use(cors());
+
 // *********  Swagger ************//
-app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+
+// ********* Redirect root to /api-docs ************ //
+app.get("/", (req, res) => {
+    res.redirect('/api-docs');
+});
 
 //  *************   Router ******************//
 app.use('/api', productRoutes)
 
-app.get("/", (req, res) => {
-    res.send("Hello Ajay")
-})
-
 
 const PORT = process.env.PORT || 5000
-const server = app.listen(PORT, (err) => {
-    if (err) {
-        console.log("Server not started")
-    } else {
-        console.log(`Server running on port ${PORT}`)
-    }
-})
+const server = app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+server.on('error', (err) => {
+    console.error('Failed to start the server:', err.message);
+});
